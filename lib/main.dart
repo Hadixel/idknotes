6eb3594bc100c6a098d34424e8a813c25acdbe7e
hadiasad <96990898+Hadiasad@users.noreply.github.com>
2025-12-30 02:25:40 +0330

    step-11

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:mynotes/views/login_view.dart';
 import 'package:mynotes/views/notes_view.dart';
 import 'package:mynotes/views/register_view.dart';
 import 'package:mynotes/views/verify_email_view.dart';
+import 'package:sqflite/sqflite.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
